06e56a0b8d553ce97df2dd734ad4c46e5e1d0c8d
v3nd3774 <josue.caraballo@gmail.com>
2018-11-10 13:23:55 -0600

    got grafana node booting, need to connect to prometheus at boot time with config still. also need to resolve gateway not booting due to missing zlib library or install pip and python from apt-get again.

--- docker/scripts/start_gateway.sh
+++ docker/scripts/start_gateway.sh
@@ -7,12 +7,12 @@ else
   echo "brokers are down..."
   exit 1
 fi
-/utilities/create_topic.sh
 /utilities/healthcheck.sh /utilities/twitter_topic_exist.sh
 echo "checking topic is up"
 if [ "$?" -eq 0 ];then
   echo "topic is up"
   /utilities/add_datanode_ip_to_hosts.sh
+  python /producer/producer.py
   tail -f /dev/null
 else
   echo "topic is down"
