dcd54f8e6a95d36094de143942704619f1b9415d
v3nd3774 <josue.caraballo@gmail.com>
2018-11-17 16:09:06 -0600

    working on connecting directory exporter to prometheus. looks to be acting up when run from k8s but ok from docker compose. just changed some stuff and not sure if this commit actually works or not.

--- docker/scripts/start_hdfs_directory_exporter.sh
+++ docker/scripts/start_hdfs_directory_exporter.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python /hdfs_directory_exporter/exporter.py /data
+/utilities/add_datanode_ip_to_hosts.sh
+python /hdfs_directory_exporter/exporter.py /data > /web_server.log 2>&1 &
+tail -f /web_server.log
